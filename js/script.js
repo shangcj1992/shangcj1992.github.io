8b53a451a9b1a6fd93d9ac531fb881c5f867feab
shangcj1992 <33414734+shangcj1992@users.noreply.github.com>
2022-02-10 15:10:02 +0800

    20220210-1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;  
     var pwd=document.getElementById("pwd").value;
-    if(userName=="🐷🐷 瑞熙 🐷🐷" &&  pwd=="123"){ 
+    if(userName=="🐷🐷 美妍 🐷🐷" &&  pwd=="123"){
 		event.preventDefault();
 		$('form').fadeOut(500);
 		$('.wrapper').addClass('form-success');
-		setTimeout(function(){location.href="Memories.html";},2000);
+		setTimeout(function(){location.href="BirthdayCake.html";},2000);
 	}
 	else{
 		alert("别瞎改我告诉你奥，改了我不通过，哼");
